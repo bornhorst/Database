bafac408e6e68c895e2d09e696a62145d836f7db
Ryan Bornhorst <bornhor2@augusta.cs.pdx.edu>
2019-06-25 13:00:48 -0700

    db prompt

---
+++ main.c
@@ -0,0 +1,16 @@
+/*
+Relational Database with Command Line
+	-functions similar to sql lite interface
+
+Travis Hermant, Ryan Bornhorst
+*/
+
+int main(int argc, char* argv[]) {
+
+/* local variables */
+
+/* variable declarations */
+
+}
+
+
