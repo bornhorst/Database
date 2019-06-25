36b24181e3341f354fea49f5a2a97dc76c16a916
Ryan Bornhorst <bornhor2@augusta.cs.pdx.edu>
2019-06-25 16:33:46 -0700

    fixing repl

--- REPL/repl.c
+++ REPL/repl.c
@@ -68,3 +68,58 @@ free(InBuffer -> Buffer);
 free(InBuffer);
 
 }
+
+/*
+Name: RunCommand
+Returns: CommandResult
+Parameters: InputBuffer*
+*/
+CommandResult RunCommand(InputBuffer* InBuffer) {
+
+// check for valid command
+if(strcmp(InBuffer -> Buffer, ".exit") == 0)
+	exit(EXIT_SUCCESS);
+else 
+	return COMMAND_FAILURE;
+
+}
+
+/*
+Name: PrepareStatment
+Returns: PrepareResult
+Parameters: InputBuffer*, Statement*
+*/
+PrepareResult PrepareStatement(InputBuffer* InBuffer, Statement* statement) {
+
+// insert 
+if(strncmp(InBuffer -> Buffer, "insert", 6) == 0) {
+	statement -> type = INSERT;
+	return PREPARE_SUCCESS;
+}
+
+// select
+if(strcmp(InBuffer -> Buffer, "select") == 0) {
+	statement -> type = SELECT;
+	return PREPARE_SUCCESS;
+}
+
+return PREPARE_FAILURE;
+
+}
+
+/*
+Name: ExecuteStatement
+Returns: void
+Parameters: Statement*
+*/
+void ExecuteStatement(Statement* statement) {
+
+// check for appropriate statements
+switch(statement -> type) {
+	case(INSERT):
+		break;
+	case(SELECT):
+		break;
+}
+
+}
