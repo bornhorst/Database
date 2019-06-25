d6597a173e36efa91d5401003feabcd211c6563c
Ryan Bornhorst <bornhor2@augusta.cs.pdx.edu>
2019-06-25 13:15:36 -0700

    initial db prompt

--- main.c
+++ main.c
@@ -5,11 +5,34 @@ Relational Database with Command Line
 Travis Hermant, Ryan Bornhorst
 */
 
+/* includes */
+#include "REPL/repl.h"
+
 int main(int argc, char* argv[]) {
 
 /* local variables */
+InputBuffer* InBuffer;
 
-/* variable declarations */
+/* initialize variables */
+InBuffer = NewInputBuffer();
+
+/* run database command prompt */
+while(true) {
+
+	// print db prompt
+	CommandPrompt();
+	
+	// grab input
+	ReadInput(InBuffer);
+
+	// follow command with action
+	if(strcmp(InBuffer -> Buffer, ".exit") == 0) {
+		CloseInputBuffer(InBuffer);
+		exit(EXIT_SUCCESS);
+	} else {
+		printf("Not a valid command '%s'.\n", InBuffer -> Buffer);
+	}
+}
 
 }
 
